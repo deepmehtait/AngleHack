12c34b0f8975c963eeb49b06f8cd2fe147922e60
David Zhang <david8zhang@berkeley.edu>
2016-06-26 05:06:20 -0700

    set up volley

--- app/src/main/java/hack/com/anglehack/constants/Constants.java
+++ app/src/main/java/hack/com/anglehack/constants/Constants.java
@@ -10,4 +10,9 @@ public class Constants {
     public static final String MY_ACCESS_KEY_ID = "AKIAI2JHWM7SN5KQGW4Q";
     public static final String MY_SECRET_KEY = "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";
 
+    // API Endpoints
+    public static final String BASE_URL = "http://ec2-54-85-202-76.compute-1.amazonaws.com:8080/api/v1/";
+    public static final String NUTRITION = BASE_URL + "/getNutrition?foodItem=";
+    public static final String PROVIDERS = BASE_URL + "/providers";
+    
 }
---
+++ app/src/main/java/hack/com/anglehack/utils/ApiManager.java
@@ -0,0 +1,16 @@
+package hack.com.anglehack.utils;
+
+/**
+ * Created by david_000 on 6/26/2016.
+ */
+public class ApiManager {
+
+    public ApiManager() {
+    }
+
+    /** Fetch a Striiv Data from Amazon Kinesis. */
+    public void fetchStriiv() {
+
+    }
+
+}
